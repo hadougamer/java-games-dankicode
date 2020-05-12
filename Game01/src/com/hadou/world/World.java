0fb26093db7fd1b670770e1c6dea5f837ecf860d
Alexandre Conrado <conrado@nasc.io>
2020-05-12 16:04:45 -0300

    Collision

--- Game01/src/com/hadou/world/World.java
+++ Game01/src/com/hadou/world/World.java
@@ -15,8 +15,9 @@ import com.hadou.main.Game;
 
 public class World {
 	
-	private Tile[] tiles;
+	private static Tile[] tiles;
 	public static int WIDTH, HEIGHT;
+	public static int TILE_SIZE=16;
 	
 	public World( String path ) {
 		try {
@@ -70,6 +71,27 @@ public class World {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static boolean isFree(int xnext, int ynext) {
+		int x1 = xnext / TILE_SIZE;
+		int y1 = ynext / TILE_SIZE;
+		
+		int x2 = (xnext+TILE_SIZE-1)/TILE_SIZE;
+		int y2 = ynext/TILE_SIZE;
+
+		int x3 = ynext/TILE_SIZE;
+		int y3 = (ynext+TILE_SIZE-1)/TILE_SIZE;
+		
+		int x4 = (ynext+TILE_SIZE-1)/TILE_SIZE;
+		int y4 = (ynext+TILE_SIZE-1)/TILE_SIZE;
+		
+		return !(
+			(tiles[x1 + (y1*World.WIDTH)] instanceof WallTile) ||
+			(tiles[x2 + (y2*World.WIDTH)] instanceof WallTile) ||
+			(tiles[x3 + (y3*World.WIDTH)] instanceof WallTile) ||
+			(tiles[x4 + (y4*World.WIDTH)] instanceof WallTile)
+		);
 	}
 	
 	public void render(Graphics g ) {
